49630e6148192138124af91497ecbe474f859f50
heihaipeng <2287714508@qq.com>
2023-10-25 15:33:56 +0800

    first commit

---
+++ src/main/java/com/nwpu/qiniumedia/QiniuMediaApplication.java
@@ -0,0 +1,13 @@
+package com.nwpu.qiniumedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QiniuMediaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QiniuMediaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nwpu/qiniumedia/QiniuMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nwpu.qiniumedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QiniuMediaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
