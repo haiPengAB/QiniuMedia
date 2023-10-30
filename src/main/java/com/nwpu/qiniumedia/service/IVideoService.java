d42a48293bd7f841b85b9a08d76d90b093eea02a
heihaipeng <2287714508@qq.com>
2023-10-30 18:57:59 +0800

    second 创建video基础代码架构

---
+++ src/main/java/com/nwpu/qiniumedia/service/IVideoService.java
@@ -0,0 +1,7 @@
+package com.nwpu.qiniumedia.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.nwpu.qiniumedia.entity.Video;
+
+public interface IVideoService extends IService<Video> {
+}
